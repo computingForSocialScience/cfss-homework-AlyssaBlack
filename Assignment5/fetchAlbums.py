e29f0f1d660257ef05ffa07d446f2e1f79bbfe9e
AlyssaBlack <ablackburn5@gmail.com>
2015-02-11 23:24:34 -0600

    adding fetchArtist.py and fetchAlbums.py

--- Assignment5/fetchAlbums.py
+++ Assignment5/fetchAlbums.py
@@ -5,11 +5,45 @@ def fetchAlbumIds(artist_id):
     """Using the Spotify API, take an artist ID and 
     returns a list of album IDs in a list
     """
-    pass
+    url = "https://api.spotify.com/v1/artists/" +artist_id+ '/albums?market=US&album_type=album'
+    r = requests.get(url).json()
+    items = r['items']
+    id = []
+    for i in range(len(items)):
+    	id.append(items[i]['id'])
+    #print id
+    return id
+    
+fetchAlbumIds('6UE7nl9mha6s8z0wFQFIZ2')
+
 
 def fetchAlbumInfo(album_id):
     """Using the Spotify API, take an album ID 
     and return a dictionary with keys 'artist_id', 'album_id' 'name', 'year', popularity'
     """
-    pass
+    url = "https://api.spotify.com/v1/albums/" +album_id
+    r = requests.get(url).json()
+    #print url
+    albuminfo = {}
+    
+    artist = r['artists'][0]
+    id = artist['id']
+    print id
+    albuminfo['artist_id'] = id
+    
+    album_id = r['id']
+    albuminfo['album_id'] = album_id
+    
+    name = r['name']
+    albuminfo['name'] = name
+    
+    year = r['release_date']
+    albuminfo['year'] = year
+    
+    popularity = r['popularity']
+    albuminfo['popularity'] = popularity
+    print albuminfo
+    return albuminfo
+    
 
+fetchAlbumInfo('4ektWErsV6EIxW0jBWq1Jn')
--- Assignment5/fetchArtist.py
+++ Assignment5/fetchArtist.py
@@ -1,17 +1,52 @@
 import sys
 import requests
 import csv
+import re
+
 
 def fetchArtistId(name):
     """Using the Spotify API search method, take a string that is the artist's name, 
     and return a Spotify artist ID.
     """
-    pass
+    url = "https://api.spotify.com/v1/search?q=" +name+ "&type=artist"
+    r = requests.get(url).json()
+    artist = r["artists"]
+    #print artist
+    items = artist['items']
+    pick = items[0]
+    id = pick['id']
+    print id
+    return id
+
+fetchArtistId('Robyn')   
 
 def fetchArtistInfo(artist_id):
     """Using the Spotify API, takes a string representing the id and
 `   returns a dictionary including the keys 'followers', 'genres', 
     'id', 'name', and 'popularity'.
     """
-    pass
+    url = "https://api.spotify.com/v1/artists/"+artist_id
+    r = requests.get(url).json()
+    artistinfo = {}
+    
+    followers = r['followers']
+    followers = followers['total']
+    artistinfo['followers'] = followers
+    
+    genres = r['genres']
+    artistinfo['genres'] = genres
+    
+    id = r['id']
+    artistinfo['id'] = id
+    
+    name = r['name']
+    artistinfo['name'] = name
+    
+    popularity = r['popularity']
+    artistinfo['popularity'] = popularity
+    print artistinfo
+    return artistinfo
+    
+fetchArtistInfo('6UE7nl9mha6s8z0wFQFIZ2')
+    
 
