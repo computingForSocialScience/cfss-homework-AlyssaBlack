8288eeea298eb804239f4d661bcc706ce42165af
AlyssaBlack <ablackburn5@gmail.com>
2015-02-19 02:17:38 -0600

    Finished Part 2

---
+++ Assignment6/analyzeNetworks.py
@@ -0,0 +1,52 @@
+import pandas as pd 
+import networkx as nx 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+def readEdgeList(filename):
+	edge = pd.read_csv(filename)
+	shape = edge.shape
+	if shape[1] != 2:
+		print "Error: contains more than 2 columns"
+		return edge[:1]
+	return edge
+
+def degree(edgeList, in_or_out):
+	if in_or_out == 'in':
+		degree = edgeList['1'].value_counts()
+	elif in_or_out == 'out':
+		degree = edgeList['0'].value_counts()
+	return degree
+	
+def combineEdgelists(edgeList1, edgeList2):
+	edgeList = [edgeList1, edgeList2]
+	edgeList = pd.concat(edgeList)
+	edgeList = edgeList.drop_duplicates()
+	return edgeList
+	
+def pandasToNetworkX(edgeList):
+	DG = nx.DiGraph()
+	for x, y in edgeList.to_records(index=False):
+		DG.add_edge(x,y)
+	return DG
+	
+def randomCentralNode(inputDiGraph):
+	centrality_dict = nx.eigenvector_centrality(inputDiGraph)
+	normed_dict = {}
+	sum = 0
+	for key, value in centrality_dict.items():
+		sum = value + sum
+	for key, value in centrality_dict.items():
+		normed_eigen = value/sum
+		normed_dict[key] = normed_eigen
+	central_node = np.random.choice(normed_dict.keys(), p = normed_dict.values())
+	return central_node
+
+if __name__ == "__main__":
+	edgeList = readEdgeList('edgeList.csv')
+	#list1 = edgeList[:50]
+	#list2 = edgeList[50:]
+	#degree(edgeList, "out")
+	#combined = combineEdgeLists(list1, list2)
+	diGraph = pandasToNetworkX(edgeList)
+	print randomCentralNode(diGraph)
